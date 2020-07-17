307b232af75c43f9aec1a21f587afb0a18c321c6
Steven Hoffman <git@fustrate.com>
2020-07-16 22:38:10 -0700

    add flair list script
    Signed-off-by: Steven Hoffman <git@fustrate.com>

---
+++ lib/flair_list.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative 'flair_bot'
+require 'csv'
+
+class FlairList < FlairBot
+  def initialize
+    raise 'Please enter a subreddit name' unless ARGV[0]
+
+    super(purpose: 'Flair List', subreddit: ARGV[0])
+
+    @users = []
+  end
+
+  def run(after: nil)
+    super
+
+    CSV.open(File.expand_path('~/flair_list.csv'), 'w', headers: %w[Name CSS Text]) do |csv|
+      @users.each { |user| csv << user }
+    end
+  end
+
+  protected
+
+  def process_flair(flair)
+    @users << [flair[:user], flair[:flair_css_class], flair[:flair_text]]
+  end
+end
+
+# We can't restart this, so start from the beginning
+FlairList.new.run
