18fb122adecebb6e322b52e7d6bedd40b6763d9c
Steven Hoffman <git@fustrate.com>
2021-08-08 11:47:48 -0700

    tweak the script
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/link_flair_list.rb
+++ lib/link_flair_list.rb
@@ -10,18 +10,17 @@ class LinkFlairList
     @bot = DefaultBot.create(purpose: 'Flair Template List')
 
     @name = ARGV[0]
-    @subreddit = @bot.session.subreddit(@name)
   end
 
   def run
-    load_link_flairs
+    pp load_link_flairs
   end
 
   protected
 
   def load_link_flairs
     @bot.with_reddit_account(@bot.name_to_subreddit(@name).account.name) do
-      @subreddit.client.get("/r/#{@name}/api/link_flair_v2").body
+      @bot.session.subreddit(@name).client.get("/r/#{@name}/api/link_flair_v2").body
     end
   end
 end
