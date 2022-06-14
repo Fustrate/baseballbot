c161ee536c4ebdf3c20ec36d6c03a3ac95218b0f
Steven Hoffman <fustrate@me.com>
2022-06-14 10:51:52 -0700

    tag deployments
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,3 +17,7 @@ set :default_env, { path: '/opt/ruby/bin:$PATH' }
 set :rbenv_ruby, File.read(File.expand_path('../.ruby-version', __dir__)).strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch :rbenv_path} #{fetch :rbenv_path}/bin/rbenv exec"
 set :rbenv_map_bins, %w[bundle gem honeybadger rake ruby]
+
+namespace :deploy do
+  after :finished, 'git:tag_release'
+end
