4f5a8d2659e7ae189f43d76c6e6704646674e5fd
Steven Hoffman <fustrate@me.com>
2022-07-26 12:01:47 -0700

    start writing actual tests
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- spec/baseballbot/template/sidebar/leaders_spec.rb
+++ spec/baseballbot/template/sidebar/leaders_spec.rb
@@ -15,15 +15,54 @@ class FakeBot < Baseballbot
 end
 
 RSpec.describe Baseballbot::Template::Sidebar::Leaders do
-  describe '#hitter_stats' do
-    it 'loads stuff' do
-      bot = Baseballbot.new(user_agent: 'Baseballbot Tests')
-      account = Baseballbot::Account.new bot:, name: 'RSpecTestBot', access: ''
-      subreddit_row = { 'name' => 'dodgers', 'team_code' => 'LAD', 'team_id' => 119, 'options' => '{}' }
-      subreddit = Baseballbot::Subreddit.new(subreddit_row, bot:, account:)
-      thing = Subject.new(subreddit:)
+  let(:bot) { Baseballbot.new(user_agent: 'Baseballbot Tests') }
+  let(:account) { Baseballbot::Account.new bot:, name: 'RSpecTestBot', access: '' }
+  let(:subreddit) do
+    Baseballbot::Subreddit.new(
+      { 'name' => 'dodgers', 'team_code' => 'LAD', 'team_id' => 119, 'options' => '{}' },
+      bot:,
+      account:
+    )
+  end
+  let(:template) { Subject.new(subreddit:) }
 
-      thing.hitter_stats
+  before { stub_requests! with_response: true }
+
+  describe '#hitter_stats' do
+    it 'loads basic hitter stats' do
+      expect(template.hitter_stats).to eq(
+        'avg' => [{ name: 'F Freeman', value: '.322' }],
+        'bb' => [{ name: 'M Muncy', value: 57 }],
+        'h' => [{ name: 'F Freeman', value: 120 }],
+        'hr' => [{ name: 'M Betts', value: 22 }],
+        'obp' => [{ name: 'F Freeman', value: '.398' }],
+        'ops' => [{ name: 'F Freeman', value: '.937' }],
+        'r' => [{ name: 'M Betts', value: 67 }],
+        'rbi' => [{ name: 'T Turner', value: 69 }],
+        'sb' => [{ name: 'T Turner', value: 17 }],
+        'slg' => [{ name: 'F Freeman', value: '.539' }],
+        'xbh' => [{ name: 'F Freeman', value: 49 }]
+      )
+    end
+  end
+
+  describe '#hitter_stats_table' do
+    it 'generates a markdown table' do
+      expect(template.hitter_stats_table(stats: %w[avg bb h hr obp ops r rbi sb slg xbh])).to eq(<<~MARKDOWN)
+        Stat|Player|Total
+        -|-|-
+        AVG|F Freeman|.322
+        BB|M Muncy|57
+        H|F Freeman|120
+        HR|M Betts|22
+        OBP|F Freeman|.398
+        OPS|F Freeman|.937
+        R|M Betts|67
+        RBI|T Turner|69
+        SB|T Turner|17
+        SLG|F Freeman|.539
+        XBH|F Freeman|49
+      MARKDOWN
     end
   end
 end
