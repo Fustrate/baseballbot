ce43336dc34e245fe206ef0ae3b9d8a12ec26ff1
Steven Hoffman <fustrate@me.com>
2022-10-16 09:34:01 -0700

    add probables_and_media template helper
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- lib/baseballbot/template/game_thread/components.rb
+++ lib/baseballbot/template/game_thread/components.rb
@@ -7,6 +7,15 @@ class Baseballbot
         def header = "#{away_team.name} (#{away_record}) @ #{home_team.name} (#{home_record})"
 
         def first_pitch = "**First Pitch**: #{start_time_local.strftime('%-I:%M %p')} at #{venue_name}"
+
+        def probables_and_media
+          table columns: %w[Team Starter TV Radio], data: [
+            [team_link(away_team), pitcher_line(probable_away_starter), away_tv, away_radio],
+            [team_link(home_team), pitcher_line(probable_home_starter), home_tv, home_radio]
+          ]
+        end
+
+        def team_link(team) = link_to(team.name, "/r/#{subreddit(team.code)}")
       end
     end
   end
