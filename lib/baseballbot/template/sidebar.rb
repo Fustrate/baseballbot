4f630c86c95a92b9446f14e6b8b1d6bde91c0642
Steven Hoffman <fustrate@me.com>
2022-10-23 15:25:34 -0700

    tweak
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- lib/baseballbot/template/sidebar.rb
+++ lib/baseballbot/template/sidebar.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-Dir.glob(File.join(File.dirname(__FILE__), 'sidebar', '*.rb')).each { require _1 }
-
 class Baseballbot
   module Template
-    class Sidebar < Base
+    class Sidebar < Template::Base
+      Dir.glob(File.join(File.dirname(__FILE__), 'sidebar', '*.rb')).each { require _1 }
+
       include Template::Sidebar::Leaders
       include Template::Sidebar::Postseason
       include Template::Sidebar::TodaysGames
