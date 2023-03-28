56ce042aaaadcd3121dad7187320ed337bf11430
Steven Hoffman <fustrate@me.com>
2023-03-27 20:15:02 -0700

    add links to test
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- spec/baseballbot/templates/game_threads/components/scoring_plays_spec.rb
+++ spec/baseballbot/templates/game_threads/components/scoring_plays_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe Baseballbot::Templates::GameThreads::Components::ScoringPlays do
 
         |Inning|Event|Score|
         |:-:|-|:-:|
-        |T2|Alec Bohm singles on a line drive to right fielder Juan Soto.   Bryce Harper scores.    Nick Castellanos to 3rd.    Alec Bohm to 2nd.  Alec Bohm advances to 2nd, on a throwing error by right fielder Juan Soto.|0-**1**|
-        |T2|Matt Vierling doubles (1) on a fly ball to right fielder Juan Soto.   Nick Castellanos scores.    Alec Bohm to 3rd.|0-**2**|
-        |T2|Edmundo Sosa singles on a fly ball to left fielder Jurickson Profar.   Alec Bohm scores.    Matt Vierling to 3rd.|0-**3**|
-        |T2|Kyle Schwarber grounds out to first baseman Brandon Drury.   Matt Vierling scores.    Edmundo Sosa to 2nd.|0-**4**|
-        |B2|Brandon Drury homers (1) on a line drive to left field.|**1**-4|
-        |B2|Josh Bell homers (2) on a fly ball to right field.|**2**-4|
-        |B5|Austin Nola singles on a line drive to right fielder Nick Castellanos.   Ha-Seong Kim scores.|**3**-4|
-        |B5|Juan Soto doubles (2) on a sharp line drive to right fielder Nick Castellanos.   Austin Nola scores.    Jurickson Profar to 3rd.|**4**-4|
-        |B5|Brandon Drury singles on a line drive to center fielder Matt Vierling.   Jurickson Profar scores.    Juan Soto scores.    Jake Cronenworth to 3rd.|**6**-4|
-        |B5|Josh Bell singles on a ground ball to right fielder Nick Castellanos.   Jake Cronenworth scores.    Brandon Drury to 3rd.|**7**-4|
+        |T2|[Alec Bohm singles on a line drive to right fielder Juan Soto.   Bryce Harper scores.    Nick Castellanos to 3rd.    Alec Bohm to 2nd.  Alec Bohm advances to 2nd, on a throwing error by right fielder Juan Soto.](https://www.mlb.com/gameday/715730/play/9)|0-**1**|
+        |T2|[Matt Vierling doubles (1) on a fly ball to right fielder Juan Soto.   Nick Castellanos scores.    Alec Bohm to 3rd.](https://www.mlb.com/gameday/715730/play/11)|0-**2**|
+        |T2|[Edmundo Sosa singles on a fly ball to left fielder Jurickson Profar.   Alec Bohm scores.    Matt Vierling to 3rd.](https://www.mlb.com/gameday/715730/play/12)|0-**3**|
+        |T2|[Kyle Schwarber grounds out to first baseman Brandon Drury.   Matt Vierling scores.    Edmundo Sosa to 2nd.](https://www.mlb.com/gameday/715730/play/13)|0-**4**|
+        |B2|[Brandon Drury homers (1) on a line drive to left field.](https://www.mlb.com/gameday/715730/play/15)|**1**-4|
+        |B2|[Josh Bell homers (2) on a fly ball to right field.](https://www.mlb.com/gameday/715730/play/16)|**2**-4|
+        |B5|[Austin Nola singles on a line drive to right fielder Nick Castellanos.   Ha-Seong Kim scores.](https://www.mlb.com/gameday/715730/play/38)|**3**-4|
+        |B5|[Juan Soto doubles (2) on a sharp line drive to right fielder Nick Castellanos.   Austin Nola scores.    Jurickson Profar to 3rd.](https://www.mlb.com/gameday/715730/play/40)|**4**-4|
+        |B5|[Brandon Drury singles on a line drive to center fielder Matt Vierling.   Jurickson Profar scores.    Juan Soto scores.    Jake Cronenworth to 3rd.](https://www.mlb.com/gameday/715730/play/43)|**6**-4|
+        |B5|[Josh Bell singles on a ground ball to right fielder Nick Castellanos.   Jake Cronenworth scores.    Brandon Drury to 3rd.](https://www.mlb.com/gameday/715730/play/44)|**7**-4|
       MARKDOWN
     end
   end
