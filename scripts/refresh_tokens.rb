ca32854448215a0e1891f9bcadb8191a0efab2a4
Steven Hoffman <git@fustrate.com>
2023-05-08 14:09:50 -0700

    make refresh_tokens run faster

--- scripts/refresh_tokens.rb
+++ scripts/refresh_tokens.rb
@@ -3,25 +3,24 @@
 require_relative 'default_bot'
 
 class RefreshTokens < DefaultBot
+  # Someone keeps removing the BaseballBot app from the RedsModerator account
+  SKIP_ACCOUNTS = %w[RedsModerator].freeze
+
   def initialize(accounts: [])
     super(purpose: 'Refresh Tokens')
 
     @account_names = accounts.map(&:downcase)
   end
 
-  def run
-    accounts.each_value do |account|
-      process_account(account)
+  def run = accounts.each_value { process_account(_1) }
 
-      sleep 5
-    end
-  end
+  protected
 
   def process_account(account)
-    return if skip_account?(account.name)
-
-    if account.access.expired?
+    if account.access.expired? && !skip?(account.name)
       refresh!(account.name)
+
+      sleep 3
     else
       puts "Skipping #{account.name}"
     end
@@ -35,5 +34,5 @@ class RefreshTokens < DefaultBot
     puts "\tError: #{e.class}"
   end
 
-  def skip_account?(name) = @account_names.any? && !@account_names.include?(name.downcase)
+  def skip?(name) = SKIP_ACCOUNTS.include?(name) || (@account_names.any? && !@account_names.include?(name.downcase))
 end
