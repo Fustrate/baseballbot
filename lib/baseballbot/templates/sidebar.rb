a72fc5b41a032ed0d0566db91a8d1a931c09314e
Steven Hoffman <fustrate@me.com>
2026-02-06 14:27:20 -0800

    start using blocks

--- lib/baseballbot/templates/sidebar.rb
+++ lib/baseballbot/templates/sidebar.rb
@@ -6,6 +6,25 @@ class Baseballbot
       include Sidebars::Components
 
       def inspect = %(#<#{self.class.name} @subreddit="#{@subreddit.name}">)
+
+      def evaluated_body
+        return super unless @blocks && !@blocks.empty?
+
+        @blocks
+          .map { block_for(it) }
+          .join("\n\n")
+      end
+
+      def block_for(block)
+        case block['type']
+        when 'division_standings'
+          Blocks::DivisionStandings.new(subreddit, template: self, **block).render
+        when 'calendar'
+          Blocks::Calendar.new(subreddit, template: self, **block).render
+        else
+          block['content']
+        end
+      end
     end
   end
 end
